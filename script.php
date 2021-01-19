68dcaff088dac1fbe2ce089deecdf30c2388c86a
alexandre-silvaa <alexandrebsilva9@gmail.com>
2021-01-19 15:52:54 -0300

    validação de campos de formulário

--- script.php
+++ script.php
@@ -4,14 +4,44 @@ $categorias = [];
 $categorias[] = 'INFANTIL';
 $categorias[] = 'ADOLESCENTE';
 $categorias[] = 'ADULTO';
-//print_r($categorias);
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-//var_dump($nome);
-//var_dump($idade);
+//restrições para nome
+if(empty($nome)){
+    echo "O campo nome não pode ser vazio";
+    return;
+}
 
+if(strlen($nome) < 3){
+    echo "O nome deve conter mais de três caracteres";
+    return;
+}
+
+if(strlen($nome) > 40){
+    echo "O nome é muito extenso";
+    return;
+}
+
+//restrições para idade
+if(strlen($idade) > 2){
+    echo "A idade deve conter até dois caracteres";
+    return;
+}
+
+if(empty($idade)){
+    echo "O campo idade não pode ser vazio";
+    return;
+}
+
+if(!is_numeric($idade)){
+    echo "Digite um valor numérico para idade";
+    return;
+}
+
+
+//condições para realizar o cadastro
 if($idade >= 6 && $idade <= 12)
 {
     for($i = 0; $i < sizeof($categorias); $i++){
